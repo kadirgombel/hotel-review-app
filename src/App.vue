67c70f499ed8ee23efa9f93d22f2973a6f5c9916
Kadir Gömbel <l1115046@std.yildiz.edu.tr>
2020-01-27 02:27:44 +0300

    remove reduntant

--- src/App.vue
+++ src/App.vue
@@ -16,16 +16,5 @@ export default {
    * The name of the application.
    */
   name: 'VueBoilerplate',
-
-  /**
-   * Fires when the app has been mounted.
-   */
-  mounted() {
-    // If the user is authenticated,
-    // fetch the data from the API
-    if (this.$store.state.auth.authenticated) {
-      this.$store.dispatch('account/find');
-    }
-  },
 };
 </script>
